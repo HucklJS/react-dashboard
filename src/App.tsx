f43f84b32fab726d0670f00eb80f5d6c29304ed4
Vasiliy <vasilij.1357@yandex.ru>
2021-03-04 13:05:55 +0300

    Show data from AJAX request

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,35 @@ import './App.css';
 import Table from "./components/Table/Table";
 
 function App() {
+  const [state, setState] = React.useState({
+    data: null,
+    error: false,
+    loading: true,
+  })
+
+  React.useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('db.json')
+        const data = await response.json()
+        console.log(data)
+        setState({
+          data,
+          error: false,
+          loading: false
+        })
+      } catch {
+        setState({
+          data: null,
+          error: true,
+          loading: false
+        })
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
       <div className="container">
@@ -11,7 +40,14 @@ function App() {
         <div className="search-form">
           <input type="text" placeholder="What test are you looking for?"/>
         </div>
-        <Table/>
+        {state.loading ?
+          <div>Loading...</div> :
+          (state.error ?
+            <div>Error happened</div> :
+              // @ts-ignore
+              <Table sites={state.data.sites} tests={state.data.tests}/>
+          )
+        }
       </div>
     </div>
   );
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
 import s from './Table.module.css'
 
-function Table() {
+type SiteType = {
+  id: number,
+  url: string
+}
+
+type TestType = {
+  id: number,
+  name: string,
+  type: string,
+  status: string,
+  siteId: number
+}
+
+type TableProps = {
+  sites: Array<SiteType>,
+  tests: Array<TestType>
+}
+
+function Table({sites, tests}: TableProps) {
+  function createSiteList(tests: TestType[]) {
+    // return ["delivery.company.com", "games.company.com", ...]
+    return tests
+      .reduce((idsSite: number[], test) => {
+        idsSite.push(test.siteId)
+        return idsSite
+      }, [])
+      .map(id => {
+        const site = sites.find(site => site.id === id)
+        if (site) {
+          const url = site.url
+          return url.match(/(\w+\.){2}com$/)?.[0]
+        }
+        return ''
+      })
+  }
+  const siteList = createSiteList(tests)
+
   return (
     <div className={s['table-wrap']}>
       <table className={s['table']}>
         <thead>
-          <tr className={s['tr'] + ' ' + s['tr-head']}>
+          <tr className={`${s['tr']} ${s['tr-head']}`}>
             <th className={s['th']}>Name</th>
             <th className={s['th']}>Type</th>
             <th className={s['th']}>Status</th>
@@ -15,24 +51,40 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-        <tr className={s['tr'] + ' ' + s['tr-content']}>
-          <td className={s['td'] + ' ' + s['name']}>Order basket redesing</td>
-          <td className={s['td'] + ' ' + s['type']}>Classic</td>
-          <td className={s['td'] + ' ' + s['status']}>Online</td>
-          <td className={s['td'] + ' ' + s['site']}>market.company.com</td>
-          <td className={s['td'] + ' ' + s['button-wrap']}>
-            <button className={s['results']}>Results</button>
-          </td>
-        </tr>
-        <tr className={s['tr'] + ' ' + s['tr-content']}>
-          <td className={s['td']}>Order basket redesing</td>
-          <td className={s['td']}>Classic</td>
-          <td className={s['td']}>Online</td>
-          <td className={s['td']}>market.company.com</td>
-          <td className={s['td']}>
-            <button>Results</button>
-          </td>
-        </tr>
+          {
+            tests.map((test, i) => {
+              return (
+                <tr className={`${s['tr']} ${s['tr-content']}`} key={test.id}>
+                  <td className={`${s['td']} ${s['name']}`}>
+                    {test.name}
+                  </td>
+                  <td className={`${s['td']} ${s['type']}`}>
+                    {test.type}
+                  </td>
+                  <td className={
+                    `${s['td']} ${s['status']} ${s[test.status.toLowerCase()]}`
+                  }>
+                    {test.status}
+                  </td>
+                  <td className={`${s['td']} ${s['site']}`}>
+                    {siteList[i]}
+                  </td>
+                  <td className={`${s['td']} ${s['button-wrap']}`}>
+                    {test.status === 'DRAFT'
+                      ?
+                      <button className={s['finalize']}>
+                        Finalize
+                      </button>
+                      :
+                      <button className={s['results']}>
+                        Results
+                      </button>
+                    }
+                  </td>
+                </tr>
+              )
+            })
+          }
         </tbody>
       </table>
     </div>
